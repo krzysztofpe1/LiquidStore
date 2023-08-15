002382b316ad4e56de84d83b29f19ae59e335b56
Krzysztof Pecyna <krzysztofpe-01@o2.pl>
2023-08-15 04:57:03 +0200

    Added Controllers.

---
+++ StoreServer/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreServer.DatabaseModels;
+using StoreServer.Services;
+
+namespace StoreServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : Controller
+    {
+        private readonly OrderDbService _service;
+        public OrderController(OrderDbService service)
+        {
+            _service = service;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<ORDER>> Get([FromQuery] int? id)
+        {
+            if (id == null) return Ok(_service.Get());
+            var item = _service.Get(id.Value);
+            if (item != null) return Ok(new List<ORDER> { item });
+            return NotFound();
+        }
+
+    }
+}
---
+++ StoreServer/Controllers/StorageController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreServer.DatabaseModels;
+using StoreServer.Services;
+
+namespace StoreServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StorageController : Controller
+    {
+        private readonly StorageDbService _service;
+        public StorageController(StorageDbService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<STORAGE>> Get([FromQuery] int? id)
+        {
+            if (id == null) return Ok(_service.Get());
+            var item = _service.Get(id.Value);
+            if (item != null) return Ok(new List<STORAGE> { item });
+            return NotFound();
+        }
+
+    }
+}
