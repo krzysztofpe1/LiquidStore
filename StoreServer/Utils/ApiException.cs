dba3377951381e1d3ac2a1bade73a119d1307cf9
Krzysztof Pecyna <krzysztofpe-01@o2.pl>
2023-08-15 04:57:55 +0200

    Added Custom Exception.

---
+++ StoreServer/Utils/ApiException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreServer.Utils
+{
+    internal class ApiException : Exception
+    {
+        public HttpStatusCode StatusCode { get; }
+        public ApiException(HttpStatusCode code, string message) :
+        base(message)
+        {
+            StatusCode = code;
+        }
+    }
+}
