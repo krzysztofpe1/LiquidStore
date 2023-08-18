50fa10a9ee3d9103e70cd1912d087bacfba637d4
krzysztofpe1 <krzysztofpe-01@o2.pl>
2023-08-18 14:59:05 +0200

    Added First View to Client

--- StoreClient/Views/LoginView.xaml.cs
+++ StoreClient/Views/LoginView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using StoreClient.Views;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,21 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace StoreClient
+namespace StoreClient.Views
 {
     /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
+    /// Interaction logic for LoginView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginView : UserControl
     {
-        #region private properties
-        private LoginView loginView;
-        #endregion
-        public MainWindow()
+        public LoginView()
         {
             InitializeComponent();
-            loginView = new LoginView();
-            MainContent.Content = loginView;
         }
     }
 }
